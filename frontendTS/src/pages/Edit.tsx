5ed6e33af222bc860a1ec40d3e9d50ddd17f2a14
Tony <128275792+tkwang312@users.noreply.github.com>
2024-09-29 23:23:01 -0400

    cleaned up commented code

--- frontendTS/src/pages/Edit.tsx
+++ frontendTS/src/pages/Edit.tsx
@@ -1,95 +1,3 @@
-// import React, { useContext, useState, useEffect, useRef } from 'react';
-// import SidebarCreate from "../components/SidebarCreate";
-// import UserContext from '../authentication/UserContext';
-// import { Button, Box, ChakraProvider, Flex, Image, SimpleGrid } from "@chakra-ui/react";
-// import ImageCropper from './ImageCropper';
-// import plus_sign from './assets/plus_sign.png';
-
-// const Edit = () => {
-//     const { selectedImage, setUpdateToggle, updateToggle } = useContext(UserContext);
-//     const [images, setImages] = useState([null, null, null, null]);
-//     const [selectedIndex, setSelectedIndex] = useState(null);
-//     const cropperRef = useRef(null);
-    
-//     useEffect(() => {
-//         if (selectedImage) {
-//             const newImages = [...images];
-//             newImages[selectedIndex] = selectedImage.photo_url;
-//             setImages(newImages);
-//         }
-//     }, [selectedImage, selectedIndex]);
-
-//     const handleDelete = () => {
-//         const newImages = [...images];
-//         newImages[selectedIndex] = null;
-//         setImages(newImages);
-//     };
-
-//     const handleCropComplete = (croppedImage) => {
-//         if (selectedIndex !== null) {
-//             const newImages = [...images];
-//             newImages[selectedIndex] = croppedImage; // Set the actual cropped image
-//             setImages(newImages);
-//         }
-//     };
-
-//     const BOX_WIDTH = 392;
-//     const BOX_HEIGHT = 550;
-
-//     return (
-//         <Flex h="100%">
-//             <Box>
-//                 <SidebarCreate />
-//             </Box>
-//             <Box as="main" flex='1' p="10px">
-//                 <ChakraProvider>
-//                     <Button onClick={handleDelete}>Remove Image</Button>
-//                     <Image src={selectedImage?.photo_url} />
-//                     <ImageCropper
-//                         ref={cropperRef}
-//                         imageProp={selectedImage}
-//                         onCropComplete={handleCropComplete}
-//                         cropWidth={BOX_WIDTH}
-//                         cropHeight={BOX_HEIGHT}
-//                     />
-//                     <Box w='850px' h='1200px' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-//                         <SimpleGrid spacing={2} columns={2} p="2px">
-//                             {images.map((img, index) => (
-//                                 <Box
-//                                     key={index}
-//                                     w='392px'
-//                                     h='550px'
-//                                     display="flex"
-//                                     alignItems="center"
-//                                     justifyContent="center"
-//                                     position="relative"
-//                                     bg='#E2E8F0'
-//                                     border={index === selectedIndex ? '5px solid #63b3ed' : 'none'}
-//                                     onClick={() => { setSelectedIndex(index); }}
-//                                 >
-//                                     {img ? (
-//                                         <ImageCropper
-//                                             ref={cropperRef}
-//                                             imageProp={img}
-//                                             onCropComplete={handleCropComplete}
-//                                             cropWidth={BOX_WIDTH}
-//                                             cropHeight={BOX_HEIGHT}
-//                                         />
-//                                     ) : (
-//                                         <img src={plus_sign} alt="plus-sign" style={{ width: '15%' }} />
-//                                     )}
-//                                 </Box>
-//                             ))}
-//                         </SimpleGrid>
-//                     </Box>
-//                 </ChakraProvider>
-//             </Box>
-//         </Flex>
-//     );
-// }
-
-// export default Edit;
-
 import { useState, useCallback, useEffect, useContext, useRef } from 'react';
 import Cropper from 'react-easy-crop';
 import { Button, Box, ChakraProvider, Flex, Image, SimpleGrid, Input } from "@chakra-ui/react";
@@ -211,109 +119,75 @@ const Edit = () => {
 
   return (
     <div>
-    <Flex h="100%">
-        <Box>
-            <SidebarCreate />
-        </Box>
-        <Box as="main" flex='1' p="10px">
-            <ChakraProvider>
-                {/* <Input type="file" accept="image/*" onChange={handleFileChange} /> */}
-                <br />
-                <Button onClick={showCroppedImage}>Show Result</Button>
-                <Button onClick={handleCrop}>Handle Crop</Button>
-                <Box w='850px' h='1200px' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                         <SimpleGrid spacing={2} columns={2} p="2px">
-                             {images.map((img, index) => (
-                                <Box
-                                    key={index}
-                                    w='392px'
-                                    h='550px'
-                                    display="flex"
-                                    alignItems="center"
-                                    justifyContent="center"
-                                    position="relative"
-                                    bg='#E2E8F0'
-                                    border={index === selectedIndex ? '5px solid #63b3ed' : 'none'}
-                                    onClick={() => { setSelectedIndex(index); }}
-                                >
-                                {!img ? (
-                                    <img src={plus_sign} alt="plus-sign" style={{ width: '15%' }} />
-                                ) : (
-                                    isCropping ? (
-                                        img && (
-                                            <div>
-                                                <h3>Crop the image below:</h3>
-                                                <Cropper
-                                                    image={img}
-                                                    crop={crop}
-                                                    zoom={zoom}
-                                                    aspect={5 / 7}
-                                                    onCropChange={setCrop}
-                                                    onCropComplete={onCropComplete}
-                                                    onZoomChange={setZoom}
-                                                    // cropSize={{ width: '392px', height: '550px' }}
-                                                />
-                                            </div>
-                                        )
+        <Flex h="100%">
+            <Box>
+                <SidebarCreate />
+            </Box>
+            <Box as="main" flex='1' p="10px">
+                <ChakraProvider>
+                    <br />
+                    <Button onClick={showCroppedImage}>Show Result</Button>
+                    <Button onClick={handleCrop}>Handle Crop</Button>
+                    <Box w='850px' h='1200px' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                            <SimpleGrid spacing={2} columns={2} p="2px">
+                                {images.map((img, index) => (
+                                    <Box
+                                        key={index}
+                                        w='392px'
+                                        h='550px'
+                                        display="flex"
+                                        alignItems="center"
+                                        justifyContent="center"
+                                        position="relative"
+                                        bg='#E2E8F0'
+                                        border={index === selectedIndex ? '5px solid #63b3ed' : 'none'}
+                                        onClick={() => { setSelectedIndex(index); }}
+                                    >
+                                    {!img ? (
+                                        <img src={plus_sign} alt="plus-sign" style={{ width: '15%' }} />
                                     ) : (
-                                        croppedImage ? (
-                                            <div>
-                                                <h3>Cropped Image Preview:</h3>
-                                                <img src={croppedImage} alt="Cropped" />
-                                            </div>
+                                        isCropping ? (
+                                            img && (
+                                                <div>
+                                                    <h3>Crop the image below:</h3>
+                                                    <Cropper
+                                                        image={img}
+                                                        crop={crop}
+                                                        zoom={zoom}
+                                                        aspect={5 / 7}
+                                                        onCropChange={setCrop}
+                                                        onCropComplete={onCropComplete}
+                                                        onZoomChange={setZoom}
+                                                        // cropSize={{ width: '392px', height: '550px' }}
+                                                    />
+                                                </div>
+                                            )
                                         ) : (
-                                            <img src={img} alt="Selected" />
+                                            croppedImage ? (
+                                                <div>
+                                                    <h3>Cropped Image Preview:</h3>
+                                                    <img src={croppedImage} alt="Cropped" />
+                                                </div>
+                                            ) : (
+                                                <img src={img} alt="Selected" />
+                                            )
                                         )
-                                    )
-                                )}
-                                </Box>
-                            ))}
-                        </SimpleGrid>
-                    </Box>
-                {croppedImage && (
-                    <div>
-                        <h3>Cropped Image Preview:</h3>
-                        <img src={croppedImage} alt="Cropped" />
-                    </div>
-                    )}
+                                    )}
+                                    </Box>
+                                ))}
+                            </SimpleGrid>
+                        </Box>
+                    {croppedImage && (
+                        <div>
+                            <h3>Cropped Image Preview:</h3>
+                            <img src={croppedImage} alt="Cropped" />
+                        </div>
+                        )}
 
 
-            </ChakraProvider>
-        </Box>
-    </Flex>
-    {/* <div className="container">
-      <div className="container-cropper">
-        <h2>Image Crop Demo</h2>
-        <input type="file" accept="image/*" onChange={handleFileChange} />
-        <br />
-        {imageSrc && (
-          <div>
-            <h3>Crop the image below:</h3>
-            <Cropper
-              image={imageSrc}
-              crop={crop}
-              zoom={zoom}
-              aspect={4 / 3}
-              onCropChange={setCrop}
-              onCropComplete={onCropComplete}
-              onZoomChange={setZoom}
-            />
-          </div>
-        )}
-
-        {croppedImage && (
-          <div>
-            <h3>Cropped Image Preview:</h3>
-            <img src={croppedImage} alt="Cropped" />
-          </div>
-        )}
-      </div>
-      <footer>
-        <button onClick={showCroppedImage}>
-          Show Result
-        </button>
-      </footer>
-    </div> */}
+                </ChakraProvider>
+            </Box>
+        </Flex>
     </div>
   );
 };

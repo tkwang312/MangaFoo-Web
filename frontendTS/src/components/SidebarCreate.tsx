a5671bb77135530a49179b4eec7c261a2d715b2f
Tony <128275792+tkwang312@users.noreply.github.com>
2024-09-30 22:45:34 -0400

    switch from react-easy-crop to react-konva for image editing

--- frontendTS/src/components/SidebarCreate.tsx
+++ frontendTS/src/components/SidebarCreate.tsx
@@ -8,7 +8,8 @@ import {
     FiUser,
     FiDollarSign,
     FiChevronLeft,
-    FiSettings
+    FiSettings,
+    FiChevronRight
 } from 'react-icons/fi'
 import UserContext from '../authentication/UserContext'
 
@@ -17,6 +18,7 @@ const SidebarCreate = () => {
     const [allPhotos, setAllPhotos] = useState([])
     const [selectedID, setSelectedID] = useState(0)
     const { uid, updateToggle, selectedImage, setSelectedImage, setUpdateToggle } = useContext(UserContext)
+    const navigate = useNavigate();
     const handleDelete = (e) => {
 
         const imageDict = selectedImage
@@ -34,7 +36,6 @@ const SidebarCreate = () => {
             setUpdateToggle(!updateToggle)
         })
     }
-    const navigate = useNavigate();
     useEffect(() => { 
         if (uid) {  
             fetch(`http://127.0.0.1:8000/images/?uid=${uid}`)  
@@ -69,139 +70,87 @@ const SidebarCreate = () => {
     };
 
 
-
+    const handleDragStart = (event, photo) => {
+        event.dataTransfer.setData('photoUrl', photo.photo_url);
+    };
+    
     return (
         <Flex>
+            {/* Sidebar navigation */}
             <Flex
                 h="1080px"
-                // pos="sticky"
-                // boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-                // borderRadius={navSize == "small" ? "15px" : "30px"}
                 w="60px"
                 flexDir="column"
                 bg="purple.200"
+                p="5%"
+                justifyContent="space-between"
             >
-                <Flex
-                    p="5%"
-                    flexDir="column"
-                    w="100%"
-                    alignItems="center"
-                    as="nav"
-                >
+                <Flex flexDir="column" alignItems="center">
+                    <IconButton
+                        mt={5}
+                        icon={navSize === "small" ? <FiChevronRight /> : <FiChevronLeft />}
+                        onClick={() => changeNavSize(navSize === "small" ? "large" : "small")}
+                        bg="purple.100"
+                    />
                     <IconButton
                         background="none"
                         mt={5}
-                        _hover={{ background: 'purple.200' }}
+                        _hover={{ background: 'purple.300' }}
                         icon={<FiMenu />}
-                        onClick={() => {
-                            if (navSize == "small")
-                                changeNavSize("large")
-                            // else
-                            //     changeNavSize("small")
-                        }}
+                        onClick={() => changeNavSize(navSize === "small" ? "large" : "small")}
                     />
-                </Flex>
-                <Flex
-                    p="5%"
-                    flexDir="column"
-                    w="100%"
-                    alignItems="center"
-                    as="nav"
-                >
                     <IconButton
                         background="none"
                         mt={5}
-                        _hover={{ background: 'purple.200' }}
+                        _hover={{ background: 'purple.300' }}
                         icon={<FiUser />}
-                        onClick={() => {
-                            if (navSize == "small")
-                                changeNavSize("large")
-                            // else
-                            //     changeNavSize("small")
-                        }}
                     />
-                    <Text size="2px">haha</Text>
-                </Flex>
-
-                <Flex
-                    p="5%"
-                    flexDir="column"
-                    w="100%"
-                    alignItems="center"
-                    as="nav"
-                >
                     <IconButton
                         background="none"
                         mt={5}
-                        _hover={{ background: 'purple.200' }}
+                        _hover={{ background: 'purple.300' }}
                         icon={<FiCalendar />}
-                        onClick={() => {
-                            if (navSize == "small")
-                                changeNavSize("large")
-                            // else
-                            //     changeNavSize("small")
-                        }}
                     />
-                </Flex>
-
-                <Flex
-                    p="5%"
-                    flexDir="column"
-                    w="100%"
-                    alignItems="center"
-                    as="nav"
-                >
                     <IconButton
                         background="none"
                         mt={5}
-                        _hover={{ background: 'purple.200' }}
+                        _hover={{ background: 'purple.300' }}
                         icon={<FiSettings />}
                         onClick={handleClickEdit}
                     />
                 </Flex>
             </Flex>
+
             <Flex
                 w={navSize == "small" ? "0px" : "400px"}
                 bg="purple.100"
                 flexBasis={'auto'}
                 overflow={'hidden'}
             >
-                <Grid  gap={1}>
-                    <GridItem as="main" colSpan="2" p="40px">
-                        <VStack align="stretch">
-                            <Heading p="10px">Pictures</Heading>
-                            <Button onClick={handleDelete}>Delete</Button>
-                            <SimpleGrid spacing={2} columns={2} p="10px">
-                                {
-                                    allPhotos.map((photo) => {
-                                        return (
-                                            <Image
-                                                key={photo.id}
-                                                h="200px"
-                                                w="150px"
-                                                src={photo.photo_url}
-                                                objectFit="cover"
-                                                border={allPhotos.findIndex(item => item['id'] === photo.id) === selectedID ? '5px solid #63b3ed' : 'none'}
-                                                onClick={() => handleImageClick(photo.id)}                                            />
-                                        )
-                                    })
-                                }
-                            </SimpleGrid>
-                        </VStack>
-                    </GridItem>
-                </Grid>
+                <VStack align="stretch">
+                    <Heading p="10px">Pictures</Heading>
+                    <Button onClick={handleDelete} isDisabled={!selectedImage}>Delete</Button>
+                    <SimpleGrid spacing={4} columns={2} p="10px">
+                        {allPhotos.map((photo) => (
+                            <Image
+                                key={photo.id}
+                                h="200px"
+                                w="150px"
+                                src={photo.photo_url}
+                                objectFit="cover"
+                                draggable="true"
+                                onDragStart={(e) => handleDragStart(e, photo)}
+                                border={allPhotos.findIndex(item => item['id'] === photo.id) === selectedID ? '5px solid #63b3ed' : 'none'}
+                                onClick={() => handleImageClick(photo.id)}
+                            />
+                        ))}
+                    </SimpleGrid>
+                </VStack>
             </Flex>
-            <IconButton
-                top="400px"
-                bg="purple.100"
-                align="left"
-                width={navSize == "small" ? "0px" : "10px"}
-                height={navSize == "small" ? "0px" : "50px"}
-                icon={navSize == "small" ? "none" : <FiChevronLeft />}
-                onClick={() => { changeNavSize("small") }}
-            />
         </Flex>
-    )
-}
+    );
+};
+    
+
 
 export default SidebarCreate

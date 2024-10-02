25b183fa8cf6f660b6a0d30c507b031829d8c86b
Tony <128275792+tkwang312@users.noreply.github.com>
2024-10-01 20:38:20 -0400

    updated generated image pixel sizes

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -6,11 +6,9 @@ from diffusers import (
 )
 from PIL import Image
 
-# Initialize LoRA model and weights
 lora_model_id = "Linaqruf/anime-nouveau-xl-lora"
 lora_filename = "anime-nouveau-xl.safetensors"
 
-# Load VAE component
 vae = AutoencoderKL.from_pretrained(
     "madebyollin/sdxl-vae-fp16-fix", 
     torch_dtype=torch.float16
@@ -18,16 +16,12 @@ vae = AutoencoderKL.from_pretrained(
 
 model_list = ["Linaqruf/animagine-xl-2.0", "eienmojiki/Anything-XL"]
 
-# Load and fuse LoRA weights
 
-
-# Define prompts and generate image
 p = "basketball, face focus, cute, masterpiece, best quality, 1girl, sketch, monochrome, greyscale, green hair, sweater, looking at viewer, upper body, beanie, outdoors, night, turtleneck"
 np = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"
 
 async def generate_image(modelID, prompt, negative_prompt, guidance_scale, inference_steps) -> Image:
 
-    # Configure the pipeline
     pipe = StableDiffusionXLPipeline.from_pretrained(
         model_list[modelID], 
         vae=vae,
@@ -42,12 +36,11 @@ async def generate_image(modelID, prompt, negative_prompt, guidance_scale, infer
     image = pipe(
         prompt, 
         negative_prompt=negative_prompt, 
-        width=1024,
-        height=1024,
+        width=512,
+        height=768,
         guidance_scale=guidance_scale,
         num_inference_steps=inference_steps
     ).images[0]
 
-    # Unfuse LoRA before saving the image
     pipe.unfuse_lora()
     return image
